9224e7b3433f2029cc74fbb41a981af7470ae1cc
Dcyii <decyismail000@gmail.com>
2025-04-18 15:06:26 +0700

    Update amplify.tf

--- amplify.tf
+++ amplify.tf
@@ -1,6 +1,6 @@
 
 resource "aws_amplify_app" "frontend" {
   name = "LKSFrontend"
-  repository = "https://github.com/Dcyii/klaten-api"
+  repository = "https://github.com/handipradana/lks-serverless/tree/main/front-end"
   iam_service_role_arn = aws_iam_role.lambda_exec.arn
 }
